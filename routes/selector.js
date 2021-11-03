779af40c1bd70a0bd47061c0fa9ede5dfca48248
kaushik-093 <89543300+kaushik-093@users.noreply.github.com>
2021-11-03 10:10:24 -0500

    final commit

--- routes/selector.js
+++ routes/selector.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Monkey', { title: 'search results for Monkey' });
+  res.render('selector', { title: 'search results for selector' });
 });
 
 module.exports = router;
+ 
